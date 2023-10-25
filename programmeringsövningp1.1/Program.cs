94e95d2ab6f7ce0b75d95989d770f61fdaa48d2f
DommSite <johan.johansson06@live.se>
2023-10-25 10:31:14 +0200

    Add project files.

---
+++ programmeringsövningp1.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace lyckhjul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random slump = new Random();
+            int vinstnummer = slump.Next(1, 11);
+            
+            Console.WriteLine("Välj en siffra mellan 1 till 10 för en chans att vinna på lyckohjulet");
+            int gissning = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hjulet stannade på " + vinstnummer);
+            if (gissning == vinstnummer)
+            {
+                Console.WriteLine("Du vann");
+            }
+            else
+            {
+                Console.WriteLine("Du vann tyvärr inte");
+            }
+        }
+    }
+}
